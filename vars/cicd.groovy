53a50a229fd3b72e544f220b00bfae0a083b5f74
kumarpille <98799172+kumarpille@users.noreply.github.com>
2022-02-19 11:14:16 +0530

    b

---
+++ vars/cicd.groovy
@@ -0,0 +1,17 @@
+def newgit(repo)
+{
+  git "${repo}" 
+} 
+def newmaven()
+{
+  sh 'mvn package' 
+}
+def deploy(ip,appname)
+{
+  sh "scp /home/ubuntu/.jenkins/workspace/descriptivepipeline2/webapp/target/webapp.war ubuntu@${ip}:/var/lib/tomcat9/webapps/${appname}.war"
+} 
+def runselenium(path)
+{
+  sh "java -jar ${path}/testing.jar"
+
+}  
