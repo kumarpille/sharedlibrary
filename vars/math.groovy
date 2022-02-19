fb80544dea11edf467bdb09b25842ffecff729e4
kumarpille <98799172+kumarpille@users.noreply.github.com>
2022-02-19 10:38:10 +0530

    a

---
+++ vars/math.groovy
@@ -0,0 +1,16 @@
+def sum(a,b)
+{
+   echo "this sum of ${a} and ${b} is ${a+b}"
+}
+def multiply(a,b)
+{
+    echo "this product of ${a} and ${b} is ${a*b}"
+}
+def subtraction(a,b)
+{
+    echo "this subtraction result of ${a} and ${b} is ${a-b}"
+}
+def divide(a,b)
+{
+    echo "this division of ${a} and ${b} is ${a/b}"
+}
